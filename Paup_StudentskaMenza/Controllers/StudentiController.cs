2af5cc9db53e693ddb4fddf5441c8175e353c253
PikaBoo <MatijaMurk@users.noreply.github.com>
2021-06-09 20:55:24 +0200

    -promjenjena boja -dodan controler StudentiController

---
+++ Paup_StudentskaMenza/Controllers/StudentiController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paup_StudentskaMenza.Controllers
+{
+    public class StudentiController : Controller
+    {
+        // GET: Studenti
+        [AllowAnonymous]
+        public ActionResult Index()
+        {
+            ViewBag.Title = "Početna stranica";
+            ViewBag.Menza = "Studentska menza";
+            return View();
+        }
+    }
+}
