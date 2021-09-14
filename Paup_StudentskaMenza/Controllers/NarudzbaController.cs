f7654c04e58fcb60626276a08e5c56499bca8b34
PikaBoo <MatijaMurk@users.noreply.github.com>
2021-09-14 23:42:07 +0200

    Kosarica

---
+++ Paup_StudentskaMenza/Controllers/NarudzbaController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Paup_StudentskaMenza.Controllers
+{
+    public class NarudzbaController : Controller
+    {
+        // GET: Narudzba
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
